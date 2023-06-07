e8668b44571a53195b21b74bc849b0749a84f3a9
FireWars <rafaelveigaramos10@gmail.com>
2023-06-06 22:35:27 -0300

    git stash

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "57509919a685ab76"; }
+/******/ 		__webpack_require__.h = function() { return "566ba3f2c62a7ddb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
