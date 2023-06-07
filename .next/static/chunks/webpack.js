e20b00469cbfe963d1a07e2b1c4feea7f894cb2e
Gabryelle Silva Santos <gabryelle-gaby@hotmail.com>
2023-06-06 22:39:03 -0300

    feat: add title

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "57509919a685ab76"; }
+/******/ 		__webpack_require__.h = function() { return "8ebdf9ac272b4c90"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
